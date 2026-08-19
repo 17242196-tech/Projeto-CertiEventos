60bd373a290508dcdf19f9bd9bdd5dd893f794bb
Isabelle <17242196@esuda.edu.br>
2026-08-19 20:07:36 -0300

    Removendo node_modules do rastreamento e atualizando projeto

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const participantesFacade = require('./participantesFacade');
 const path = require('path');
 
 const app = express();
+const cors = require('cors');
+app.use(cors());
+
 app.use(express.json());
 
 // ---------------- CONFIG FRONTEND ---------------- //
